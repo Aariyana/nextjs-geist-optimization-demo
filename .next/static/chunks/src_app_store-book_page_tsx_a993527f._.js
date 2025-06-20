df77028f53fd8b230f4723cbb1dd7f1b32ac5efa
Aariyana <48056044+Aariyana@users.noreply.github.com>
2025-06-20 21:51:48 +0530

    Add .next/static/chunks/src_app_store-book_page_tsx_a993527f._.js 1750436508213

---
+++ .next/static/chunks/src_app_store-book_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_801196d4._.js",
+  "static/chunks/node_modules_05649cdc._.js"
+],
+    source: "dynamic"
+});
